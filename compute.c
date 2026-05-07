be0ba5fc868705328c09618c40858fc9f840c23f
macklin <macklin@udel.edu>
2026-05-06 21:24:08 -0400

    Fixed compute.c

--- compute.c
+++ compute.c
@@ -2,48 +2,100 @@
 #include <math.h>
 #include "vector.h"
 #include "config.h"
+#include <cuda_runtime.h>
+
+#define EPSILON 1e-9
+// Kernel for computing gravitational forces
+__global__ void compute_gravitational_forces(vector3* pos, double* mass, vector3* accels, int N) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+	if (i >= N || j >= N) return;
+
+	
+
+	if (i ==j) {
+		FILL_VECTOR(accels[i*N + j], 0,0,0);
+		return;
+	}
+
+	vector3 distance;
+    double magnitude_sq = 0.0;
+
+    for (int k = 0; k < 3; k++) {
+        distance[k] = pos[i][k] - pos[j][k];
+        magnitude_sq += distance[k] * distance[k];
+    }
+
+    if (magnitude_sq < EPSILON) return;
+
+    double magnitude = sqrt(magnitude_sq);
+
+    double accelmag = -GRAV_CONSTANT * mass[j] / magnitude_sq;
+
+    FILL_VECTOR(
+        accels[i * N + j],
+        accelmag * distance[0] / magnitude,
+        accelmag * distance[1] / magnitude,
+        accelmag * distance[2] / magnitude
+    );
+}
+// Kernel for reducing the gravitational forces
+__global__ void reduce_accels(vector3* accels, vector3* accel_sum, int N) {
+
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	if (i >= N) return;
+
+	vector3 sum = {0, 0, 0};
+	for (int j = 0; j < N; j++) {
+		for (int k = 0; k < 3; k++) {
+			sum[k] += accels[i * N + j][k];
+		}
+	}
+ accel_sum[i] = sum;
+}
 
 //compute: Updates the positions and locations of the objects in the system based on gravity.
 //Parameters: None
 //Returns: None
 //Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
 void compute(){
-	//make an acceleration matrix which is NUMENTITIES squared in size;
-	int i,j,k;
-	vector3* values=(vector3*)malloc(sizeof(vector3)*NUMENTITIES*NUMENTITIES);
-	vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
-	for (i=0;i<NUMENTITIES;i++)
-		accels[i]=&values[i*NUMENTITIES];
-	//first compute the pairwise accelerations.  Effect is on the first argument.
-	for (i=0;i<NUMENTITIES;i++){
-		for (j=0;j<NUMENTITIES;j++){
-			if (i==j) {
-				FILL_VECTOR(accels[i][j],0,0,0);
-			}
-			else{
-				vector3 distance;
-				for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
-				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
-				double magnitude=sqrt(magnitude_sq);
-				double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
-				FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
-			}
-		}
-	}
-	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
-	for (i=0;i<NUMENTITIES;i++){
-		vector3 accel_sum={0,0,0};
-		for (j=0;j<NUMENTITIES;j++){
-			for (k=0;k<3;k++)
-				accel_sum[k]+=accels[i][j][k];
-		}
-		//compute the new velocity based on the acceleration and time interval
-		//compute the new position based on the velocity and time interval
-		for (k=0;k<3;k++){
-			hVel[i][k]+=accel_sum[k]*INTERVAL;
-			hPos[i][k]+=hVel[i][k]*INTERVAL;
-		}
-	}
-	free(accels);
-	free(values);
+	 int N = NUMENTITIES;
+
+    vector3 *d_pos, *d_accels, *d_accel_sum;
+    double *d_mass;
+
+    cudaMalloc(&d_pos, sizeof(vector3) * N);
+    cudaMalloc(&d_accels, sizeof(vector3) * N * N);
+    cudaMalloc(&d_mass, sizeof(double) * N);
+    cudaMalloc(&d_accel_sum, sizeof(vector3) * N);
+
+    cudaMemcpy(d_pos, hPos, sizeof(vector3) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_mass, mass, sizeof(double) * N, cudaMemcpyHostToDevice);
+
+    dim3 threads(16, 16);
+    dim3 blocks((N + 15) / 16, (N + 15) / 16);
+
+    compute_gravitational_forces<<<blocks, threads>>>(d_pos, d_mass, d_accels, N);
+    cudaDeviceSynchronize();
+
+    reduce_accels<<<(N + 255) / 256, 256>>>(d_accels, d_accel_sum, N);
+    cudaDeviceSynchronize();
+
+    vector3* accel_sum = (vector3*)malloc(sizeof(vector3) * N);
+
+    cudaMemcpy(accel_sum, d_accel_sum, sizeof(vector3) * N, cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < N; i++) {
+        for (int k = 0; k < 3; k++) {
+            hVel[i][k] += accel_sum[i][k] * INTERVAL;
+            hPos[i][k] += hVel[i][k] * INTERVAL;
+        }
+    }
+
+    cudaFree(d_pos);
+    cudaFree(d_accels);
+    cudaFree(d_mass);
+    cudaFree(d_accel_sum);
+    free(accel_sum);
 }
